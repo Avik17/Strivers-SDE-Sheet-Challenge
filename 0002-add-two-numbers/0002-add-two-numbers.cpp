5e386c7ac392e7c95e42e612147e72fcc60d1d7a
Avinash Kumar <76001307+Avik17@users.noreply.github.com>
2023-06-20 14:43:50 +0530

    Time: 44 ms (39%), Space: 71.5 MB (8.36%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -23,7 +23,7 @@ public:
     ListNode* addTwoNumbers(ListNode* h1, ListNode* h2) {
         // ListNode* h1=reverseList(l1);
         // ListNode* h2=reverseList(l2);
-        bool carry_done=false;
+    //    bool carry_done=false;
         int carry=0;
         int val=0;
         ListNode* ans=NULL;
@@ -34,69 +34,42 @@ public:
           
            if(h1==NULL){
               val=h2->val+carry;
-              carry_done=true;
+
               h2=h2->next;
-               carry=0;
+               
            }
            else  if(h2==NULL){
               val=h1->val+carry;
-              carry_done=true;
+         
               h1=h1->next;
-               carry=0;
+            
              
            }
           else  
             {
               val=h1->val+h2->val+carry;
-           carry_done=true;
+          
             h1=h1->next;
             h2=h2->next;
-            carry=0;
+            
             }
+            carry=0;
              if(val>=10)
             {
             carry=val/10;
             val=val%10;
-            carry_done=false;
-            
             }
             
             insertNode(ans,val);
-            if(h1==NULL && h2==NULL && carry!=0 && !carry_done )
+            if(h1==NULL && h2==NULL && carry!=0  )
              {  
                
                insertNode(ans, carry);
              }
             
         }
-      
-        return ans;
+            return ans;
     }
 
-
-
-
-  
-    ListNode* reverseList(ListNode* head) {
-        
-    if(head==NULL || head->next==NULL)
-      return head; 
- 
-    ListNode* prev=head;
-    ListNode* curr=head->next;
-      ListNode* temp=curr->next;
-    while(temp!=NULL)
-    {
-        curr->next=prev;
-        prev=curr;
-        curr=temp;
-        temp=temp->next;
-        
-    }
-    curr->next=prev;
-    head->next=NULL;
-    head=curr;
- return head;
-    }
 
 };
