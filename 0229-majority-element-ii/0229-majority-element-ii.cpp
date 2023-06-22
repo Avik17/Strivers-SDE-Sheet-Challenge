11960828b7a1487058daa0e74911b554865b61b9
Avinash Kumar <76001307+Avik17@users.noreply.github.com>
2023-06-22 14:18:39 +0530

    Time: 4 ms (99.19%), Space: 16 MB (30.87%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
            map<int,int> cnt;
            vector<int> ans;
-           map<int,bool>pushed;
+
         int n=nums.size();
         
         for(int i =0 ; i<n;i++){
             cnt[nums[i]]++;
-            if(cnt[nums[i]]>n/3 && !pushed[nums[i]]){
-            ans.push_back(nums[i]);
-            pushed[nums[i]]=true;
-            }
         }
+        for(auto num : cnt){
+            if( num.second > n/3)
+                ans.push_back(num.first);
+            }
       return ans;
         
     }
