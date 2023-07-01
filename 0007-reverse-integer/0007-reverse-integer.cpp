ad000dcceddc3ae2e86d06b986abfee173f4ee71
Avinash Kumar <76001307+Avik17@users.noreply.github.com>
2023-07-01 21:53:02 +0530

    Time: 0 ms (100%), Space: 5.9 MB (48.81%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string num=to_string(x);
+        int j=num.length()-1;
+        int i=0;
+       long ans=x;
+       
+        if(ans>=0)
+        while(i<j){
+            swap(num[i++],num[j--]);
+        }
+        else {
+        ans=-ans;
+         while(i<j){
+            swap(num[i++],num[j--]);
+        }
+        }
+        ans=stol(num);
+        if(ans<INT_MIN || ans>INT_MAX)
+        return 0;
+      if(x<0)
+      ans=-ans;
+      return ans;
+    }
+};
