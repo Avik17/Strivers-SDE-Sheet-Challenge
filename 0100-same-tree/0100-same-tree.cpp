fa4a1e2fee98a9567ca8862c1daeceb9c88358e0
Avinash Kumar <76001307+Avik17@users.noreply.github.com>
2023-08-11 23:43:55 +0530

    Time: 0 ms (100%), Space: 9.9 MB (63.08%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -16,14 +16,12 @@ public:
       
     }
   bool check(TreeNode* root1,TreeNode* root2) {
-     if(root1==NULL && root2!=NULL)
-     return false;
-       if(root2==NULL && root1!=NULL)
-     return false;
-        if(root1==NULL && root2==NULL)
+     if(!root1 && !root2)
         return true;
-        if(root1->val==root2->val)
-    return check(root1->left,root2->left) && check(root1->right,root2->right);
- else return false;
+       if(!root2 || !root1)
+     return false;
+
+    return (root1->val==root2->val) && check(root1->left,root2->left) && check(root1->right,       root2->right);
+ return false;
     }
 };
